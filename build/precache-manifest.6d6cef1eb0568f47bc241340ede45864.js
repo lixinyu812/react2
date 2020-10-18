65dd7d5e70419b3fc4162b6de0f13314c2b04c9e
李欣雨 <45160675+lixinyu812@users.noreply.github.com>
2020-10-18 16:26:38 +0800

    Add files via upload

---
+++ build/precache-manifest.6d6cef1eb0568f47bc241340ede45864.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d10073db658fafa515c190c31bca0826",
+    "url": "/index.html"
+  },
+  {
+    "revision": "021ae38be79fd898d3f4",
+    "url": "/static/css/main.a3a8b971.chunk.css"
+  },
+  {
+    "revision": "b568cdcac3bcc06ddff5",
+    "url": "/static/js/2.02780966.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.02780966.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "021ae38be79fd898d3f4",
+    "url": "/static/js/main.85963194.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
